2f96362de2a395aa281876b19b72c509efbc3c77
Punit Naik <naik.punit44@gmail.com>
2025-06-11 23:52:37 +0530

    :gift: init

---
+++ README.md
@@ -0,0 +1,10 @@
+# clj-dsa
+
+Data Structures and Algorithms implementation in Clojure
+
+## License
+
+Copyright © 2025 [Punit Naik](https://github.com/punit-naik)
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-dsa "1.0.0"
+  :description "Data Structures and Algorithms implementation in Clojure"
+  :url "https://www.github.com/punit-naik/clj-dsa.git"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.12.1"]])
